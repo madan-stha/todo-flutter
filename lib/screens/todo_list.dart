95c38506c903b6c4d1fa14aaffa26fe9daf29531
MadanStha7 <madanstha.124@gmail.com>
2023-12-03 21:37:19 +0545

    new commit

---
+++ lib/screens/todo_list.dart
@@ -0,0 +1,154 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:todo/screens/add_page.dart';
+import 'package:http/http.dart' as http;
+
+import '../services/todo_service.dart';
+
+class TodoListPage extends StatefulWidget {
+  const TodoListPage({super.key});
+
+  @override
+  State<TodoListPage> createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends State<TodoListPage> {
+  bool isLoading = true;
+  List items = [];
+  @override
+  void initState() {
+    super.initState();
+    fetchTodo();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text('Todo List'),
+        ),
+      ),
+      body: Visibility(
+        visible: isLoading,
+        replacement: RefreshIndicator(
+          onRefresh: fetchTodo,
+          child: Visibility(
+            visible: items.isNotEmpty,
+            replacement: Center(
+                child: Text(
+              'No Todo Item',
+              style: Theme.of(context).textTheme.headline3,
+            )),
+            child: ListView.builder(
+                itemCount: items.length,
+                padding: const EdgeInsets.all(12),
+                itemBuilder: (context, index) {
+                  final item = items[index] as Map;
+                  final id = item['_id'];
+                  return Card(
+                    child: ListTile(
+                      leading: CircleAvatar(child: Text('${index + 1}')),
+                      title: Text(item['title']),
+                      subtitle: Text(item['description']),
+                      trailing: PopupMenuButton(onSelected: (value) {
+                        if (value == 'edit') {
+                          // Open Edit Page
+                          navigateToEditPage(item);
+                        } else if (value == 'delete') {
+                          // Delete and remove the item
+                          deleteById(id);
+                        }
+                      }, itemBuilder: (context) {
+                        return [
+                          const PopupMenuItem(
+                            value: 'edit',
+                            child: Text('Edit'),
+                          ),
+                          const PopupMenuItem(
+                            value: 'delete',
+                            child: Text('Delete'),
+                          ),
+                        ];
+                      }),
+                    ),
+                  );
+                }),
+          ),
+        ),
+        child: const Center(child: CircularProgressIndicator()),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: navigateToAddPage,
+        label: const Text('Add Todo'),
+      ),
+    );
+  }
+
+  Future<void> navigateToAddPage() async {
+    final route = MaterialPageRoute(
+      builder: (context) => const AddTodoPage(),
+    );
+    await Navigator.push(context, route);
+    setState(() {
+      isLoading = true;
+    });
+    fetchTodo();
+  }
+
+  Future<void> navigateToEditPage(Map item) async {
+    final route = MaterialPageRoute(
+      builder: (context) => AddTodoPage(todo: item),
+    );
+    await Navigator.push(context, route);
+    setState(() {
+      isLoading = true;
+    });
+    fetchTodo();
+  }
+
+  Future<void> deleteById(String id) async {
+    //delete the item
+
+    final isSuccess = await TodoService.deleteById(id);
+    if (isSuccess) {
+      //remove item from list
+      final filtered = items.where((element) => element['_id'] != id).toList();
+      setState(() {
+        items = filtered;
+      });
+    } else {
+      //show error
+      showErrorMessage('Deletion Failed');
+    }
+  }
+
+  Future<void> fetchTodo() async {
+    const url = 'http://api.nstack.in/v1/todos?page=1&limit=10';
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+
+    if (response.statusCode == 200) {
+      final json = jsonDecode(response.body) as Map;
+      final result = json['items'] as List;
+      setState(() {
+        items = result;
+      });
+    }
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  void showErrorMessage(String message) {
+    final snackBar = SnackBar(
+      content: Text(
+        message,
+        style: const TextStyle(color: Colors.white),
+      ),
+      backgroundColor: Colors.red,
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+}
---
+++ lib/services/todo_service.dart
@@ -0,0 +1,11 @@
+import 'package:http/http.dart' as http;
+
+// All todo api call will be here
+class TodoService {
+  static Future<bool> deleteById(String id) async {
+    final url = 'http://api.nstack.in/v1/todos/$id';
+    final uri = Uri.parse(url);
+    final response = await http.delete(uri);
+    return response.statusCode == 200;
+  }
+}
